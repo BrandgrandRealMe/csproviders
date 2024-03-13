745a264ff3edcec8fdf48d0cb8ee0bc294edcf99
BrandgrandRealMe <brandon@brandgrand.tech>
2024-03-13 02:16:06 -0500

    Test 1

--- FlixwaveProvider/build.gradle.kts
+++ FlixwaveProvider/build.gradle.kts
@@ -5,8 +5,8 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    description = "A provider for Flixwave."
+    authors = listOf("BrandgrandReal")
 
     /**
     * Status int as the following:
--- FlixwaveProvider/src/main/kotlin/com/flixwaveprovider/FlixwavePlugin.kt
+++ FlixwaveProvider/src/main/kotlin/com/flixwaveprovider/FlixwavePlugin.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.flixwaveprovider
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
